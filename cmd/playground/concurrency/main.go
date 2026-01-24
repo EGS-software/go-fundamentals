6f3909d1cb613bd02ca9e3a76b6ee824038e2a96
JaoVicy <filipimjoa@gmail.com>
2026-01-24 10:29:03 -0300

    feat: created another concurrency test with counters

--- cmd/playground/concurrency/main.go
+++ cmd/playground/concurrency/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
@@ -10,4 +11,14 @@ func main() {
 	}()
 
 	fmt.Println("Fim da Main") // Is faster than the "go func" above
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Printf("Hello from goroutine!")
+	}()
+	wg.Wait()
+	fmt.Printf("The End!")
 }
