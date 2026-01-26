5e53cb3677a10e7a6c6ab3e62114b5ba48abe15c
JaoVicy <filipimjoa@gmail.com>
2026-01-26 19:41:19 -0300

    feat add Process func

--- internal/structures/structs/PaymentSlip.go
+++ internal/structures/structs/PaymentSlip.go
@@ -1,6 +1,15 @@
 package structs
 
+import (
+	"fmt"
+)
+
 type PaymentSlip struct {
 	id     int
 	amount float64
+}
+
+func (p PaymentSlip) Process(data string) string {
+	fmt.Print("Starting Payment Slip Processing...\n")
+	return fmt.Sprintf("Payment Slip Processed: %s", data)
 }
