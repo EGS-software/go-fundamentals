b94b64bd107f59afac49b3dd1a47dac055332c8a
JaoVicy <filipimjoa@gmail.com>
2026-01-26 19:41:34 -0300

    feat add CalcData func

--- internal/structures/structs/PaymentSlip.go
+++ internal/structures/structs/PaymentSlip.go
@@ -13,3 +13,7 @@ func (p PaymentSlip) Process(data string) string {
 	fmt.Print("Starting Payment Slip Processing...\n")
 	return fmt.Sprintf("Payment Slip Processed: %s", data)
 }
+
+func (p PaymentSlip) CalcData(value float64) float64 {
+	return value * 1.05
+}
