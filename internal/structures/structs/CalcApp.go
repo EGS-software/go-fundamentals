7d2b4adcadb9e9dca393e3097d448544e894acc8
JaoVicy <filipimjoa@gmail.com>
2026-01-28 20:05:28 -0300

    feat: created internal func

--- internal/structures/structs/CalcApp.go
+++ internal/structures/structs/CalcApp.go
@@ -2,10 +2,19 @@ package structs
 
 import (
 	"setup/internal/structures/interfaces"
+	"strconv"
 )
 
 type CalcApp struct {
 	Processor interfaces.Processor
 	Id        int
 	Name      string
+}
+
+func (c CalcApp) CalcApp(value float64) string {
+	resultText := c.Processor.Process("Data for Payment Slip")
+	calc := c.Processor.CalcData(value)
+
+	result := "Name: " + resultText + " | " + "CalcValue: " + strconv.FormatFloat(calc, 'f', 2, 64)
+	return result
 }
