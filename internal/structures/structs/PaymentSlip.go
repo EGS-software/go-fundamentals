88e2dacf3890ad1698ec6449cc8aaabf0634fb23
JaoVicy <filipimjoa@gmail.com>
2026-01-29 19:29:02 -0300

    style: change comment and string retunr

--- internal/structures/structs/PaymentSlip.go
+++ internal/structures/structs/PaymentSlip.go
@@ -10,7 +10,7 @@ type PaymentSlip struct {
 	Amount float64
 }
 
-// Implementing the Processor interface methods for PaymentSlip
+// Process Implementing the Processor interface methods for PaymentSlip
 func (p PaymentSlip) Process(Data string) string {
 	fmt.Print("Starting Payment Slip Processing...\n")
 	return fmt.Sprintf("Payment Slip Processed: %s", Data)
@@ -19,7 +19,7 @@ func (p PaymentSlip) Process(Data string) string {
 // CalcData method implementation
 func (p PaymentSlip) CalcData(Value float64) (float64, error) {
 	if Value < 0 {
-		return 0.0, errors.New("Value must be greater than zero")
+		return 0.0, errors.New("value must be greater than zero")
 	}
 
 	return Value * 1.05, nil // Example: adding 5% processing fee
