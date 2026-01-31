8ec4fa3fffee54ce12443a1fc22a8aa8439734c9
JaoVicy <filipimjoa@gmail.com>
2026-01-31 11:22:02 -0300

    test: create test func to 'good scenario'

--- internal/structures/structs/payment_slip_test.go
+++ internal/structures/structs/payment_slip_test.go
@@ -1,1 +1,25 @@
 package structs
+
+import (
+	"testing"
+)
+
+// The name of func NEED to start with Test and receive (t *testing.T)
+func TestPaymentSlip_CalcData(t *testing.T) {
+	// Scenario 1: Good return
+	t.Run("Must to be calc 5%: ", func(t *testing.T) {
+		slip := PaymentSlip{Id: 1, Amount: 100.0}
+
+		// Call the method
+		got, err := slip.CalcData(slip.Amount)
+		want := 105.0 // 100 * 1.05
+
+		// Verifications
+		if err != nil {
+			t.Errorf("Not wait error, but come: %v", err)
+		}
+		if got != want {
+			t.Errorf("Wait %.2f, but come %.2f", want, got)
+		}
+	})
+}
