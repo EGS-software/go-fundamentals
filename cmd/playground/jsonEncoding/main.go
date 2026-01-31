0208e39386b8af55bc6ebf494d55936df419c9e7
JaoVicy <filipimjoa@gmail.com>
2026-01-31 15:20:58 -0300

    feat: add func to transform data in json

--- cmd/playground/jsonEncoding/main.go
+++ cmd/playground/jsonEncoding/main.go
@@ -1,1 +1,19 @@
-package jsonEncoding
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"setup/internal/structures/structs"
+)
+
+func main() {
+	paymentSlip := structs.PaymentSlip{Id: 1, Amount: 100.0}
+
+	jsonData, err := json.Marshal(paymentSlip)
+	if err != nil {
+		fmt.Println("Error to generate JSON:", err)
+		return
+	}
+
+	fmt.Println("JSON:", string(jsonData))
+}
